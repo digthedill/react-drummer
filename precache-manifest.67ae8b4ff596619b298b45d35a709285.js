95ee4a9db43ee49f52e450f2130029d562d1e376
digthedill <popssly@gmail.com>
2020-08-25 15:21:08 -0500

    Updates

---
+++ precache-manifest.67ae8b4ff596619b298b45d35a709285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31902914d448fe65cac06e0012d9242d",
+    "url": "/react-drummer/index.html"
+  },
+  {
+    "revision": "d056bd76de24ea55bc2b",
+    "url": "/react-drummer/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "a67c5b0f33aae5ce6657",
+    "url": "/react-drummer/static/css/main.0c4d8816.chunk.css"
+  },
+  {
+    "revision": "d056bd76de24ea55bc2b",
+    "url": "/react-drummer/static/js/2.6d87c83a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-drummer/static/js/2.6d87c83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67c5b0f33aae5ce6657",
+    "url": "/react-drummer/static/js/main.edf909bb.chunk.js"
+  },
+  {
+    "revision": "0553e4767d5f6a481cfb",
+    "url": "/react-drummer/static/js/runtime-main.68306a22.js"
+  }
+]);
